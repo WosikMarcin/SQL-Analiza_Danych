9ef4ee27921e66c82b7c747b4058afa9b169d074
WosikMarcin <38767544+WosikMarcin@users.noreply.github.com>
2023-01-11 21:04:48 +0100

    Update Analiza_Danych.sql

--- Analiza_Danych.sql
+++ Analiza_Danych.sql
@@ -1,3 +1,24 @@
+--------------------------------------------------------TABELE---------------------------------------------------------
+1). Utwórz tabele my_first_table a nastepnie:
+a). dodaj kolumny first_col ( typ danych text ) i second_col ( typ danych datetime )
+b). usuń tabele my_first_table
+
+a).
+CREATE TABLE IF NOT EXISTS my_first_table(
+    first_col text,
+    second_col datetime
+)
+b). DROP TABLE my_first_table
+
+2). Utwórz tabele actor_nick a nastepnie:
+a). wypełnij tabele na podstawie zapytania, ktore ma wyswietlic wszystkie dane z sakila.actor gdzie first_name to Nick 
+b). usuń tabele actor_nick
+
+CREATE TABLE IF NOT EXISTS actor_nick AS
+SELECT * FROM sakila.actor
+WHERE first_name = 'Nick'
+
+
 ------------------------------------------------------GRUPOWANIE--------------------------------------------------------
 1).Zagreguj tabelę payment według następujących reguł:
 
